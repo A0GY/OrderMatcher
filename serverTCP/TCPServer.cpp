2c81813cdb3ed21275d2bb96e3e5eb08f9dbfec9
A0GY <alialiyassine8@gmail.com>
2026-07-29 13:29:51 +0100

    Moving buffer within clients lifetime so new client does not inherite any leftover bytes in buffere

--- serverTCP/TCPServer.cpp
+++ serverTCP/TCPServer.cpp
@@ -12,7 +12,7 @@
 // TCP enigne to be atached to Matching engine 
 int main(){
 auto live_ts = std::chrono::system_clock::now();
-std::array<char,500> _buffer ;
+//std::array<char,500> _buffer ;
 std::string stringBuffer;
 std::string CommandLine;
 MatchingEngine engine;
@@ -65,6 +65,7 @@ if (listen(sockfd.get(), 10) == -1) {perror("listen");
 return 1;}
 
 while (true){
+
 Socket connfd ( accept(sockfd.get() ,nullptr,nullptr));
 if (connfd.get() == -1){perror("accept");
 return 1;}
@@ -74,6 +75,7 @@ send (connfd.get(), msg, strlen(msg), 0);
 bool keep_client = true;
 // loop over to take input from client,, not all bytes may have been moved from kernel to buffer to we need to loop over 
 while (keep_client == true){
+std::array<char,500> _buffer ; // moving to within client lifecycle to clear buffer once a client has     
 ssize_t _recv = recv(connfd.get(),_buffer.data(),_buffer.size(),0);
 
 if (_recv == -1){ perror("recv");
